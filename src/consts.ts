8814f865ad7c4e28abd3b43cc8ea645c07afae14
Angel Nieves <nievesaj@gmail.com>
2023-07-01 11:15:12 -0400

    site title update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'ZimmyThat 🤖';
+export const SITE_TITLE = 'KeyboardKB ⌨️';
 export const SITE_DESCRIPTION = 'Welcome to the ZimmyThat Website!';
